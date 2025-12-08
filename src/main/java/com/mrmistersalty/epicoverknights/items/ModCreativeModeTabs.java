155fbd538e17a6da9c5a50529d33af05fecc009d
MrMisterSalty <mister.salty@tutanota.com>
2025-12-08 18:24:24 +0100

    [Feat] Added Stylet Blades (without recipe)

--- src/main/java/com/mrmistersalty/epicoverknights/items/ModCreativeModeTabs.java
+++ src/main/java/com/mrmistersalty/epicoverknights/items/ModCreativeModeTabs.java
@@ -12,6 +12,13 @@ public class ModCreativeModeTabs {
   @SubscribeEvent
   public static void onBuildCreativeTab(BuildCreativeModeTabContentsEvent event) {
     if (event.getTabKey().location().equals(new ResourceLocation("overgeared", "overgeared_tab"))) {
+      event.accept(ModItems.COPPER_STYLET_BLADE.get());
+      event.accept(ModItems.GOLD_STYLET_BLADE.get());
+      event.accept(ModItems.TIN_STYLET_BLADE.get());
+      event.accept(ModItems.STONE_STYLET_BLADE.get());
+      event.accept(ModItems.SILVER_STYLET_BLADE.get());
+      event.accept(ModItems.BRONZE_STYLET_BLADE.get());
+      event.accept(ModItems.IRON_STYLET_BLADE.get());
       event.accept(ModItems.STEEL_STYLET_BLADE.get());
     }
   }
--- src/main/java/com/mrmistersalty/epicoverknights/items/ModItems.java
+++ src/main/java/com/mrmistersalty/epicoverknights/items/ModItems.java
@@ -11,6 +11,13 @@ public class ModItems {
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
       EpicOverKnights.MODID);
 
+  public static final RegistryObject<Item> COPPER_STYLET_BLADE = ITEMS.register("copper_stylet_blade", ()-> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> GOLD_STYLET_BLADE = ITEMS.register("gold_stylet_blade", ()-> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> TIN_STYLET_BLADE = ITEMS.register("tin_stylet_blade", ()-> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> STONE_STYLET_BLADE = ITEMS.register("stone_stylet_blade", ()-> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> SILVER_STYLET_BLADE = ITEMS.register("silver_stylet_blade", ()-> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> BRONZE_STYLET_BLADE = ITEMS.register("bronze_stylet_blade", ()-> new Item(new Item.Properties()));
+  public static final RegistryObject<Item> IRON_STYLET_BLADE = ITEMS.register("iron_stylet_blade", ()-> new Item(new Item.Properties()));
   public static final RegistryObject<Item> STEEL_STYLET_BLADE = ITEMS.register("steel_stylet_blade", ()-> new Item(new Item.Properties()));
 
   public static void register(IEventBus bus) {
