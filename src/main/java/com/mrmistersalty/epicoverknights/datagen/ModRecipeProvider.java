5add2be27b363bae4ac9693c964971c41660fdbb
MrMisterSalty <mister.salty@tutanota.com>
2026-02-02 13:00:23 +0100

    Feat: integrating DataGen

---
+++ src/main/java/com/mrmistersalty/epicoverknights/datagen/ModRecipeProvider.java
@@ -0,0 +1,25 @@
+package com.mrmistersalty.epicoverknights.datagen;
+
+import com.mrmistersalty.epicoverknights.items.ModItems;
+import java.util.function.Consumer;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.stirdrem.overgeared.datagen.ShapedForgingRecipeBuilder;
+
+public class ModRecipeProvider extends RecipeProvider {
+  public ModRecipeProvider(PackOutput output) {
+    super(output);
+  }
+
+  @Override
+  protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
+    ShapedForgingRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.COPPER_STYLET_BLADE.get(), 3)
+        .requiresBlueprint(true)
+        .setBlueprint("stylet")
+        .setPolishing(true)
+        .setQuality(true)
+        .define('#', )
+  }
+}
